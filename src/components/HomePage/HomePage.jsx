5eceb702df4907b3f283f29c109e740100617faf
ShirinNurullahova <sirin.nurullahova@mail.ru>
2023-03-24 09:36:12 +0400

    link edited

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -19,15 +19,24 @@ const HomePage = () => {
                     <div className='footer-left'>
                         <p><span>Bizimlə əlaqə:</span>  +994 50 443 19 29 </p>
                         <div className='dots'></div>
-                        <p >info@claradix.com</p>
+                        <p>info@claradix.com</p>
                         <div className='dot'></div>
                     </div>
                     <div className='footer-right'>
-                    <p>Instagram</p>
+                        <a href='https://instagram.com/claradix_llc?igshid=YmMyMTA2M2Y=' target='_blank' rel="noreferrer">
+                           <p>Instagram</p>  
+                        </a>
+                       
                         <div className='white'></div>
-                        <p>Facebook</p>
+                        <a href='https://www.facebook.com/profile.php?id=100076390094082&mibextid=LQQJ4d' target='_blank' rel="noreferrer">
+                           <p>Facebook</p>  
+                        </a>
+                       
                         <div className='white'></div>
-                        <p>Linkedin</p>
+                        <a href='https://www.linkedin.com/company/claradix/' target='_blank' rel="noreferrer">
+                             <p>Linkedin</p>
+                        </a>
+                       
                     </div>
                 </div>
 
